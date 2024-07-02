5266f2c108dae8e6462da9977655f18894b6fca2
Tushil40 <tushilhadiya40@gmail.com>
2024-07-02 11:45:54 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { cart, product } from '../data-type';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+ popularProducts:undefined|product[];
+ trendyProducts:undefined | product[];
+  productData: any;
+  productQuantity: any;
+  router: any;
+  removeCart: boolean | undefined;
+  constructor(private product:ProductService) {}
+
+  ngOnInit(): void {
+    this.product.popularProducts().subscribe((data)=>{
+      this.popularProducts=data;
+    })
+
+    this.product.trendyProducts().subscribe((data)=>{
+      this.trendyProducts=data;
+    })
+  }
+
+}
+
+
+
---
+++ src/app/seller-home/seller-home.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-seller-home',
+  templateUrl: './seller-home.component.html',
+  styleUrls: ['./seller-home.component.css'],
+})
+export class SellerHomeComponent implements OnInit {
+  productList: undefined | product[];
+  productMessage: undefined | string;
+  icon = faTrash;
+  iconEdit=faEdit;
+  constructor(private product: ProductService, private route:Router) {}
+
+  ngOnInit(): void {
+    this.list();
+  }
+  
+
+  deleteProduct(id: number) {
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'Product is deleted';
+
+        this.list();
+      }
+    });
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
+  }
+
+  list() {
+    this.product.productList().subscribe((result) => {
+      if (result) {
+        this.productList = result;
+      }
+    });
+  }
+  addProduct(){
+    this.route.navigate(['/seller-add-product']);
+  }
+}
